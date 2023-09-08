32e5dd2eaec03ef26f30dd1a6630e800493fc4a4
jackisading <jackding.yj@gmail.com>
2023-09-07 20:06:50 -0400

    login pass

--- login.js
+++ login.js
@@ -1,2 +1,14 @@
 let vh = window.innerHeight * 0.01;
 document.documentElement.style.setProperty('--vh', `${vh}px`);
+
+let login = document.querySelector('.loginForm');
+login.addEventListener('submit', (e) => {
+    e.preventDefault();
+    let pass = document.querySelector('.formText');
+    if (pass.value.toLowerCase() != 'show') {
+        console.log('incorrect')
+    } else {
+        console.log('correct')
+        location.replace('index.html')
+    }
+})
