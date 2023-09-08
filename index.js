6675c618c03fd5565e14c0801b444e61abc73a61
jackisading <jackding.yj@gmail.com>
2023-09-08 04:11:51 -0400

    pages

--- index.js
+++ index.js
@@ -1,68 +1,14 @@
-const audioPlayer = document.querySelector(".audioContainer");
-const audio = new Audio("intro.m4a");
-console.dir(audio);
+let vh = window.innerHeight * 0.01;
+document.documentElement.style.setProperty('--vh', `${vh}px`);
 
-// set the correct audio length
-audio.addEventListener(
-    "loadeddata",
-    () => {
-      audioPlayer.querySelector(".length").textContent = getTimeCodeFromNum(
-        audio.duration
-      );
-      audio.volume = 1;
-    },
-    false
-  );
-
-//click on timeline to skip around
-const timeline = audioPlayer.querySelector(".timeline");
-timeline.addEventListener("click", e => {
-  const timelineWidth = window.getComputedStyle(timeline).width;
-  const timeToSeek = e.offsetX / parseInt(timelineWidth) * audio.duration;
-  audio.currentTime = timeToSeek;
-}, false);
-
-//check audio percentage and update time accordingly
-setInterval(() => {
-    const progressBar = audioPlayer.querySelector(".progress");
-    progressBar.style.width = audio.currentTime / audio.duration * 100 + "%";
-    console.log(audio.currentTime, audio.duration, audio.currentTime / audio.duration * 100 + "%")
-    audioPlayer.querySelector(".current").textContent = getTimeCodeFromNum(
-      audio.currentTime
-    );
-  }, 10);
-
-//toggle between playing and pausing on button click
-const playBtn = audioPlayer.querySelector(".play-pause");
-playBtn.addEventListener(
-  "click",
-  () => {
-    if (audio.paused) {
-        playBtn.src = 'pause.png';
-        audio.play();
+let login = document.querySelector('.loginForm');
+login.addEventListener('submit', (e) => {
+    e.preventDefault();
+    let pass = document.querySelector('.formText');
+    if (pass.value.toLowerCase() != 'show') {
+        console.log('incorrect')
     } else {
-        playBtn.src = 'play.png';
-        audio.pause();
+        console.log('correct')
+        location.replace('page1.html')
     }
-  },
-  false
-);
-
-//when audio finishes, reset icon
-audio.addEventListener("ended", function(){
-    playBtn.src = 'play.png';
-});
-
-//turn 128 seconds into 2:08
-function getTimeCodeFromNum(num) {
-    let seconds = parseInt(num);
-    let minutes = parseInt(seconds / 60);
-    seconds -= minutes * 60;
-    const hours = parseInt(minutes / 60);
-    minutes -= hours * 60;
-  
-    if (hours === 0) return `${minutes}:${String(seconds % 60).padStart(2, 0)}`;
-    return `${String(hours).padStart(2, 0)}:${minutes}:${String(
-      seconds % 60
-    ).padStart(2, 0)}`;
-  }
+})
---
+++ page7.js
@@ -0,0 +1,68 @@
+const audioPlayer = document.querySelector(".audioContainer");
+const audio = new Audio("assets/dance.m4a");
+console.dir(audio);
+
+// set the correct audio length
+audio.addEventListener(
+    "loadeddata",
+    () => {
+      audioPlayer.querySelector(".length").textContent = getTimeCodeFromNum(
+        audio.duration
+      );
+      audio.volume = 1;
+    },
+    false
+  );
+
+//click on timeline to skip around
+const timeline = audioPlayer.querySelector(".timeline");
+timeline.addEventListener("click", e => {
+  const timelineWidth = window.getComputedStyle(timeline).width;
+  const timeToSeek = e.offsetX / parseInt(timelineWidth) * audio.duration;
+  audio.currentTime = timeToSeek;
+}, false);
+
+//check audio percentage and update time accordingly
+setInterval(() => {
+    const progressBar = audioPlayer.querySelector(".progress");
+    progressBar.style.width = audio.currentTime / audio.duration * 100 + "%";
+    console.log(audio.currentTime, audio.duration, audio.currentTime / audio.duration * 100 + "%")
+    audioPlayer.querySelector(".current").textContent = getTimeCodeFromNum(
+      audio.currentTime
+    );
+  }, 10);
+
+//toggle between playing and pausing on button click
+const playBtn = audioPlayer.querySelector(".play-pause");
+playBtn.addEventListener(
+  "click",
+  () => {
+    if (audio.paused) {
+        playBtn.src = 'assets/pause.png';
+        audio.play();
+    } else {
+        playBtn.src = 'assets/play.png';
+        audio.pause();
+    }
+  },
+  false
+);
+
+//when audio finishes, reset icon
+audio.addEventListener("ended", function(){
+    playBtn.src = 'assets/play.png';
+});
+
+//turn 128 seconds into 2:08
+function getTimeCodeFromNum(num) {
+    let seconds = parseInt(num);
+    let minutes = parseInt(seconds / 60);
+    seconds -= minutes * 60;
+    const hours = parseInt(minutes / 60);
+    minutes -= hours * 60;
+  
+    if (hours === 0) return `${minutes}:${String(seconds % 60).padStart(2, 0)}`;
+    return `${String(hours).padStart(2, 0)}:${minutes}:${String(
+      seconds % 60
+    ).padStart(2, 0)}`;
+  }
